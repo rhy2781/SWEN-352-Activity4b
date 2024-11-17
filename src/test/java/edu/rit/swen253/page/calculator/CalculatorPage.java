fb29038bd4645188458c1842774a776b747d2d19
Robert Yamasaki <rhy2781@rit.edu>
2024-11-17 14:11:53 -0500

    almost complete

--- src/test/java/edu/rit/swen253/page/calculator/CalculatorPage.java
+++ src/test/java/edu/rit/swen253/page/calculator/CalculatorPage.java
@@ -1,14 +1,9 @@
 package edu.rit.swen253.page.calculator;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import edu.rit.swen253.page.AbstractAngularPage;
 import edu.rit.swen253.utils.DomElement;
@@ -19,10 +14,16 @@ public class CalculatorPage extends AbstractAngularPage {
         super("gpa-calc");
     }
 
-    public void addCourse(){
+    public void resetPage(){
+        DomElement button = angularView.findChildBy(By.className("secondaryButton"));
+        button.click();
+    }
+    public void addCourse(int counter){
         DomElement buttonParent = angularView.findChildBy(By.className("buttonRow"));
         DomElement button = buttonParent.findChildBy(By.className("secondaryButton"));
-        button.click();
+        for(int i = 0; i < counter; i++){
+            button.click();
+        }
     }
 
     public List<Course> getCourseList(){
@@ -33,11 +34,21 @@ public class CalculatorPage extends AbstractAngularPage {
             .map(domElement -> new Course(domElement))
             .collect(Collectors.toList());
     }
+    /**
+     * The Term GPA is stored within the "results" className, for which there are two values
+     * The first element corresponds to the Term GPA
+     * @return the GPA for the term
+     */
     public String getTermGPA(){
         List<DomElement> results = angularView.findChildrenBy(By.className("results"));
         return results.get(0).getText();
     }
 
+        /**
+     * The Term GPA is stored within the "results" className, for which there are two values
+     * The second element corresponds to the Cumulative GPA
+     * @return the GPA for the term
+     */
     public String getCumulativeGPA(){
         List<DomElement> results = angularView.findChildrenBy(By.className("results"));
         return results.get(1).getText();
--- src/test/java/edu/rit/swen253/test/calculator/CalculatorTest.java
+++ src/test/java/edu/rit/swen253/test/calculator/CalculatorTest.java
@@ -46,15 +46,63 @@ public class CalculatorTest extends AbstractWebTest{
         calculatorPage = assertNewPage(CalculatorPage::new);
     }
 
-
     private static String c1Name = "SWEN 352";
-    private static String c1Credits = "4";
+    private static String c1Credits = "3";
     private static String c1GradeOption = "A";
     private static String c1GradeValue = "1: 4.00";
 
+    private static String c2Name = "CSCI 261";
+    private static String c2Credits = "3";
+    private static String c2GradeOption = "D";
+    private static String c2GradeValue = "10: 1.00";
+
+    private static String c3Name = "BIOL 140";
+    private static String c3Credits = "3";
+    private static String c3GradeOption = "F";
+    private static String c3GradeValue = "12: 0.00";
+
+    private static String c4Name = "MATH 190";
+    private static String c4Credits = "3";
+    private static String c4GradeOption = "C+";
+    private static String c4GradeValue = "6: 2.33";
 
     @Test
     @Order(3)
+    @DisplayName("Test Add/Delete Functionality")
+    void addDeleteCourses(){
+        List<Course> courses = calculatorPage.getCourseList();
+        assertEquals(1, courses.size());
+
+        calculatorPage.addCourse(2);
+        courses = calculatorPage.getCourseList();
+        assertEquals(3, courses.size());
+
+        // insert details for courses 
+        Course first = courses.get(0);
+        first.insertCourseName(c1Name);
+        Course second = courses.get(1);
+        second.insertCourseName(c2Name);
+        Course third = courses.get(2);
+        third.insertCourseName(c2Name);
+
+        // delete the first two courses
+        first.deleteCourse();
+        second.deleteCourse();
+
+        // get updated list of courses, and verify that the single one that remains 
+        // is the third course that was added and it is the only one too
+        courses = calculatorPage.getCourseList();
+        assertEquals(1, courses.size());
+        assertEquals(c2Name, courses.get(0).getCourseName());
+
+        // reset the page for the next test
+        calculatorPage.resetPage();
+        assertEquals(1, calculatorPage.getCourseList().size());
+    }
+
+
+    @Test
+    @Order(4)
     @DisplayName("Test w/ one course")
     /** Insert one course and verify that the GPA calculator is correct */
     void addCourseDetails(){
@@ -76,8 +124,66 @@ public class CalculatorTest extends AbstractWebTest{
         assertEquals("4.00", calculatorPage.getCumulativeGPA());
         
         // prepare for next test by resetting courses
-        first.deleteCourse();
-        calculatorPage.addCourse();
+        calculatorPage.resetPage();
         assertEquals(1, courses.size());
+    }
+
+
+    @Test
+    @Order(5)
+    @DisplayName("Test w/ full course schedule")
+    void addCourseDetailsFull(){
+        // add courses until 4 total
+        calculatorPage.addCourse(3);
+        List<Course> courses = calculatorPage.getCourseList();
+
+        // insert the first course details
+        Course first = courses.get(0);
+        first.insertCourseName(c1Name);
+        first.insertCourseGrade(c1GradeOption);
+        first.insertCourseCredits(c1Credits);
+        assertEquals(c1Name, first.getCourseName());
+        assertEquals(c1GradeValue, first.getCourseGrade());
+        assertEquals(c1Credits, first.getCourseCredits());
+
+        // insert the second course details
+        Course second = courses.get(1);
+        second.insertCourseName(c2Name);
+        second.insertCourseGrade(c2GradeOption);
+        second.insertCourseCredits(c2Credits);
+        assertEquals(c2Name, second.getCourseName());
+        assertEquals(c2GradeValue, second.getCourseGrade());
+        assertEquals(c2Credits, second.getCourseCredits());
+
+        // insert the third course details
+        Course third = courses.get(2);
+        third.insertCourseName(c3Name);
+        third.insertCourseGrade(c3GradeOption);
+        third.insertCourseCredits(c3Credits);
+        assertEquals(c3Name, third.getCourseName());
+        assertEquals(c3GradeValue, third.getCourseGrade());
+        assertEquals(c3Credits, third.getCourseCredits());
+
+        // insert the fourth course details
+        Course fourth = courses.get(3);
+        fourth.insertCourseName(c4Name);
+        fourth.insertCourseGrade(c4GradeOption);
+        fourth.insertCourseCredits(c4Credits);
+        assertEquals(c4Name, fourth.getCourseName());
+        assertEquals(c4GradeValue, fourth.getCourseGrade());
+        assertEquals(c4Credits, fourth.getCourseCredits());
+
+        // check the term and cumulative gpa
+        calculatorPage.calculateGPA();
+        assertEquals("1.83", calculatorPage.getTermGPA());
+        assertEquals("1.83", calculatorPage.getCumulativeGPA());
+        
+        // reset to one empty course
+        calculatorPage.resetPage();
+        courses = calculatorPage.getCourseList();
+        for(Course c: courses){
+            System.out.println(c.getCourseName());
+        }
+        assertEquals(1, calculatorPage.getCourseList().size());
     }
 }
